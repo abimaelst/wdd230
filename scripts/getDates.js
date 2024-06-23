079a2c690bb8b0a0ea2a845e5867cd5046714c87
Abimael Tavares <tavars@abimael.dev.br>
2024-06-23 09:56:40 -0300

    fix html tag

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -2,9 +2,5 @@ window.onload = () => {
     document.querySelector('#year').textContent = new Date().getFullYear();
 
     document.querySelector('#lastModified').textContent = `Last modified at ${document.lastModified}`
-    
-    console.log(document.lastModified)
-    
-    console.log('here')
-}
+ }
 
