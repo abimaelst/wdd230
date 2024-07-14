24e8cbb4bc66d10048a82b1b953507699233bb6d
Abimael Tavares <tavars@abimael.dev.br>
2024-07-14 16:14:30 -0300

    finished activity 5

--- scripts/links.js
+++ scripts/links.js
@@ -1,11 +1,34 @@
-const baseURL = "https://abimaelst.github.io/wdd230/"
+const baseURL = "https://abimaelst.github.io/wdd230/";
 
 const linksURL = "https://abimaelst.github.io/wdd230/data/links.json";
 
 async function getLinks() {
-    const response = await fetch(linksURL);
-    const data = await response.json();
-    console.log(data);
+  const response = await fetch(linksURL);
+  const data = await response.json();
+  console.log(data);
+
+  listElement = document.querySelector("#weeks-activity");
+
+  if (listElement) {
+    data.weeks.forEach((item) => {
+      const li = document.createElement("li");
+
+      let linksHTML = "";
+
+      item.links.map((link, index) => {
+        linksHTML += `<a href="${link.url}">${link.title}</a> ${
+          index + 1 < item.links.length ? " | " : ""
+        } `;
+        return;
+      });
+
+      li.innerHTML = `
+                <span>${item.week}:</span>
+                ${linksHTML}
+            `;
+      listElement.appendChild(li);
+    });
   }
-  
-  getLinks();
+}
+
+getLinks();
